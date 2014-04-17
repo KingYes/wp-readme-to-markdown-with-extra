954d9a439536c3c9ee5b31c1391c359b3e8d49e9
Yakir Sitbon <kingyes1@gmail.com>
2014-04-17 12:00:18 +0000

    Added more extra things

--- tasks/wp_readme_to_markdown.js
+++ tasks/wp_readme_to_markdown.js
@@ -14,9 +14,13 @@ module.exports = function(grunt) {
   // creation: http://gruntjs.com/creating-tasks
 
  grunt.registerMultiTask('wp_readme_to_markdown', 'Converts WP readme.txt file to markdown (readme.md)', function() {
- 
+    var options = this.options({
+		travisUrlRepo: '',
+		travisBrach: 'master',
+		gruntIcon: true,
+		gruntDependencyStatusUrl: ''
+	});
 	this.files.forEach(function(f) {
-
 		// Concat specified files.
 		var readme = f.src.filter(function(filepath) {
 			// Warn on and remove invalid source files (if nonull was set).
@@ -24,7 +28,7 @@ module.exports = function(grunt) {
 				grunt.log.warn('Source file "' + filepath + '" not found.');
 				return false;
 			} else {
-				return true; 
+				return true;
 			}
 		}).map(function(filepath) {
 			// Read file source.
@@ -32,30 +36,53 @@ module.exports = function(grunt) {
 		}).join(grunt.util.normalizelf(' '));
 
 		/* The following is a ported version of {@see https://github.com/benbalter/WP-Readme-to-Github-Markdown}*/
-
-		//Convert Headings 
+		var addToHeaderArray = [];
+		if ( '' !== options.travisUrlRepo ) {
+			addToHeaderArray.push( '[![Build Status](' + options.travisUrlRepo + '.svg?branch=' + options.travisBrach + ')](' + options.travisUrlRepo + ')' );
+		}
+		
+		if ( '' !== options.gruntDependencyStatusUrl ) {
+			addToHeaderArray.push( '[![Dependency Status](' + options.gruntDependencyStatusUrl + '/dev-status.svg)](' + options.gruntDependencyStatusUrl + '#info=devDependencies)' );		
+		}
+		
+		if ( options.gruntIcon ) {
+			addToHeaderArray.push( '[![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)' );
+		}
+		
+		var testToHeader = '';
+		for ( i = 1; i <= addToHeaderArray.length; i++ ) {
+			if ( '' !== testToHeader ) {
+				testToHeader += ' ';
+			}
+			testToHeader += addToHeaderArray[ i - 1 ];
+		}
+		
+		if ( '' !== testToHeader ) {
+			testToHeader += "\n\n";
+		}
+		
+		//Convert Headings
 		grunt.log.debug("Converting headings");
-		readme = readme.replace( new RegExp("^=([^=]+)=*?[\s ]*?$","gim"),"###$1###");	
-		readme = readme.replace( new RegExp("^==([^=]+)==*?[\s ]*?$","mig"),"##$1##");
-		readme = readme.replace( new RegExp("^===([^=]+)===*?[\s ]*?$","gim"),"#$1#");
+		readme = readme.replace( new RegExp("^=([^=]+)=*?[\s ]*?\n","gim"),"###$1###\n" );
+		readme = readme.replace( new RegExp("^==([^=]+)==*?[\s ]*?\n","mig"),"##$1##\n" );
+		readme = readme.replace( new RegExp("^===([^=]+)===*?[\s ]*?\n","gim"),"#$1#\n"  + testToHeader );
 
 		//parse contributors, donate link, etc.
 		grunt.log.debug("Parse contributors, donate link etc");
-		readme = readme.replace( new RegExp("^([^:\r\n\*]{1}[^:\r\n#\\]\\[]+): (.+)","gim"),"**$1:** $2  ");
+		readme = readme.replace( new RegExp("^([^:\n\*]{1}[^:\n#\\]\\[]+): (.+)","gim"),"**$1:** $2  ");
 
 		//guess plugin slug from plugin name
 		//@todo Get this from config instead?
 		grunt.log.debug("Get plugin name");
-		var _match =  readme.match( new RegExp("^#([^#]+)#[\s ]*?$","im") );	
+		var _match =  readme.match( new RegExp("^#([^#]+)#[\s ]*?\n","i") );
 
 		//process screenshots, if any
 		grunt.log.debug("Get screenshots");
 		var screenshot_match = readme.match( new RegExp("## Screenshots ##([^#]*)","im") );
 		if ( _match && screenshot_match && screenshot_match.length > 1 ) {
-			
 			var plugin = _match[1].trim().toLowerCase().replace(/ /g, '-');
-	
-			//Collect screenshots content	
+
+			//Collect screenshots content
 			var screenshots = screenshot_match[1];
 
 			//parse screenshot list into array
@@ -66,7 +93,7 @@ module.exports = function(grunt) {
 				nonGlobalMatch = globalMatch[i].match(  new RegExp( "^[0-9]+\. (.*)", 'im' ) );
 				matchArray.push( nonGlobalMatch[1] );
 			}
-		
+
 			//replace list item with markdown image syntax, hotlinking to plugin repo
 			//@todo assumes .png, perhaps should check that file exists first?
 			for( i=1; i <= matchArray.length; i++ ) {
@@ -75,7 +102,7 @@ module.exports = function(grunt) {
 		}
 		// Write the destination file.
 		grunt.file.write( f.dest, readme );
-	
+
 		// Print a success message.
 		grunt.log.writeln('File "' + f.dest + '" created.');
 	});
